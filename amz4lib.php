bb29d405809bcca5b5d0479136ecb18738085e78
Ken Irwin <kirwin@wittenberg.edu>
2014-11-03 10:44:34 -0500

    first commit

---
+++ amz4lib.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<?php
+   include ("amz4lib.php");
+?>
+
